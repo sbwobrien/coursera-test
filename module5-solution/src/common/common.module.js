6e0bd381d44cac7b0b6313507db82873089d27c4
sobrien <simon@obrien.com>
2016-10-23 22:11:56 +1030

    updated fro http to https

--- module5-solution/src/common/common.module.js
+++ module5-solution/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'http://rocky-fjord-57717.herokuapp.com')
+.constant('ApiPath', 'https://rocky-fjord-57717.herokuapp.com')
 .config(config);
 
 config.$inject = ['$httpProvider'];
